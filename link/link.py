658080d6f1cfa3fa85724af44d16a9a799ddee6a
Кокорев Сергей Юрьевич <SKok@uwca.ru>
2022-11-08 16:27:33 +0300

    Added getter and setter CS_position method for dataclass Link, which stores the Dual Quaternion of the coordinate system position for Link_i in the coordinate system of Link_i-1

--- link/link.py
+++ link/link.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 
 from mathlib.dual_quaternion import DualQuaternion as DQ
 from mathlib.quaternion import Quaternion as Q
+from mathlib.vector import scalar_vector
 
 
 @dataclass
@@ -12,8 +13,19 @@ class Link:
     alpha: float = 0.0
     d: float = 0.0
     teta: float = 0.0
-    link_number: int = 1
+    number: int = 1
+    name: str = 'link',
+    joint_type: str = 'rotation'
 
-    initial_position = DQ(D0=Q(), D1=Q())
-    end_postion = DQ(D0=Q(), D1=Q())
+    __CS_position = DQ()
 
+    @property
+    def CS_position(self) -> DQ:
+        return self.__CS_position
+
+    @CS_position.setter
+    def CS_position(self, cs: DQ):
+        if isinstance(cs, DQ):
+            self.__CS_position = cs
+        else:
+            print('CS_postion wron parameter')
--- test_link.py
+++ test_link.py
@@ -4,6 +4,7 @@ import math
 from link.link import Link
 from mathlib.dual_quaternion import DualQuaternion as DQ
 from mathlib.quaternion import Quaternion as Q
+from mathlib.vector import scalar_vector
 
 
 if __name__ == "__main__":
@@ -14,7 +15,7 @@ if __name__ == "__main__":
         })
     """
     DH = {
-        'link1': {'a': 0.0, 'alpha': math.pi/2, 'd': 0.0, 'teta': 0.0},
+        'link1': {'a': 0.0, 'alpha': math.pi/2, 'd': 0.1615, 'teta': 0.0},
         'link2': {'a': -0.24355, 'alpha': 0.0, 'd': 0.0, 'teta': 0.0},
         'link3': {'a': -0.2132, 'alpha': 0.0, 'd': 0.0, 'teta': 0.0},
         'link4': {'a': 0.0, 'alpha': math.pi/2, 'd': 0.13105, 'teta': 0.0},
@@ -23,9 +24,38 @@ if __name__ == "__main__":
     }
     links = []
     for i, (link, dh) in enumerate(DH.items(), 1):
-        links.append(Link(
-            a=dh['a'], alpha=dh['alpha'], d=dh['d'], teta=dh['teta'], link_number=i
-        ))
+        dh['number'], dh['name'] = i, link
+        links.append(Link(**dh))
+
+    for i, link in enumerate(links):
+        DQTrZ = DQ(
+            D0=Q(scalar=1.0), 
+            D1=Q(vector=scalar_vector(scalar=link.d/2, vector=[0.0, 0.0, 1.0]))
+        )
+        DQRotZ = DQ(
+            D0=Q(scalar=math.cos(link.teta/2), 
+                vector=scalar_vector(scalar=math.sin(link.teta/2), vector=[0.0, 0.0, 1.0])),
+            D1=Q()
+        )
+        DQTrX = DQ(
+            D0=Q(scalar=1.0), 
+            D1=Q(vector=scalar_vector(scalar=link.a/2, vector=[1.0, 0.0, 0.0]))
+        )
+        DQRotX = DQ(
+            D0=Q(scalar=math.cos(link.alpha/2), 
+                vector=scalar_vector(scalar=math.sin(link.alpha/2), vector=[1.0, 0.0, 0.0])),
+            D1=Q()
+        )
+        DQSum = DQTrZ.mult(DQRotZ).mult(DQTrX).mult(DQRotX)
+        link.CS_position = DQSum.mult(DQ(D0=Q(scalar=1.0), D1=Q())).mult(DQSum.conjugate())
 
     for link in links:
-        print(link)
+        print(f'Link {link.number}:\t{link.name}')
+        print(link.CS_position)
+        if link.number == 1:
+            DQEndEffector = link.CS_position
+        else:
+            DQEndEffector = link.CS_position.mult(DQEndEffector)
+
+    print("DQ End Effector")
+    print(DQEndEffector)
